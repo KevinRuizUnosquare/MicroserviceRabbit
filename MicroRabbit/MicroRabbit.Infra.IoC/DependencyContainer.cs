006aa769caafa7aeac08973e9c06d5e2553b32cc
kevin.ruiz <kevin.ruiz@unosquare.com>
2023-01-11 13:50:48 -0600

    Initial Commit of Infrastrcuture Dependency Project

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domian Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
